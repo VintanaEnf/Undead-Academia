696e31a98fd8ab528e262a7fd092832c6c8c4022
jeffrsnblrdoo <iamjeffersonbelardo@gmail.com>
2024-03-09 18:54:12 +0800

    New game button is now working

--- Assets/Scenes/Title Screen/main-menu.cs
+++ Assets/Scenes/Title Screen/main-menu.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void NewGame();
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //All of these examples loads "level 0"
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
 }
