5e033b2ec47dc81486f56a0c46bfdb486adabbc8
Kronoox <aldrincristobal22@gmail.com>
2024-03-13 20:45:50 +0800

    Added Player Animation

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,17 +6,30 @@ public class PlayerScript : MonoBehaviour
 {
     Rigidbody2D rb;
     public float MovementSpeed;
+    Animator animator;
+    SpriteRenderer sr;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float InputV = Input.GetAxis("Vertical");
-        float InputH = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(InputH*MovementSpeed, InputV*MovementSpeed);
+        float InputV = Input.GetAxisRaw("Vertical");
+        float InputH = Input.GetAxisRaw("Horizontal");
+
+        if (InputH != 0) rb.velocity = new Vector2(InputH * MovementSpeed, 0);
+        else if (InputV != 0) rb.velocity = new Vector2(0, InputV * MovementSpeed);
+        else rb.velocity = new Vector2(0, 0);
+
+        animator.SetFloat("yVelocity", rb.velocity.y);
+        animator.SetFloat("xVelocity", rb.velocity.x);
+
+        if (rb.velocity.x > 0) sr.flipX = true;
+        else if (rb.velocity.x <0) sr.flipX = false;
     }
 }
