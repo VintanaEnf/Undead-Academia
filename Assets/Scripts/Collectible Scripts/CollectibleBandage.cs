43c7bb7314f442e488dcac3f23b59787dc6c1398
MichaelJustineDJ <161811588+MichaelJustineDJ@users.noreply.github.com>
2024-03-15 22:18:59 +0800

    Bandage and Battery Pickable

---
+++ Assets/Scripts/Collectible Scripts/CollectibleBandage.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectibleBandage : MonoBehaviour
+{
+    Animator pickedUp;
+
+    public Image EButton;
+    public Boolean AllowPickup;
+    
+    void Start()
+    {
+        pickedUp = GetComponent<Animator>();
+        EButton.gameObject.SetActive(false);
+    }
+    
+    void Update()
+    {
+        if (AllowPickup && Input.GetKeyDown(KeyCode.E)) 
+        {
+            pickUp();
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            EButton.gameObject.SetActive(true);
+            AllowPickup = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {
+            EButton.gameObject.SetActive(false);
+            AllowPickup = false;
+        }
+    }
+    
+    private void pickUp()
+    {
+        pickedUp.SetBool("IsPickedUp", true);
+        Destroy(gameObject, 0.7f);
+    }
+}
---
+++ Assets/Scripts/Collectible Scripts/CollectibleBattery.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectbleBattery : MonoBehaviour
+{
+    Animator pickedUp;
+
+    public Image EButton;
+    public Boolean AllowPickup;
+
+    
+    void Start()
+    {
+        pickedUp = GetComponent<Animator>();
+        EButton.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AllowPickup && Input.GetKeyDown(KeyCode.E))
+        {
+            pickUp();
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            EButton.gameObject.SetActive(true);
+            AllowPickup = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            EButton.gameObject.SetActive(false);
+            AllowPickup = false;
+        }
+    }
+
+    private void pickUp()
+    {
+        pickedUp.SetBool("IsPickedUp", true);
+        Destroy(gameObject, 0.7f);
+    }
+}
