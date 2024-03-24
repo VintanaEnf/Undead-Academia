e83ae58d093aae756a8f54050d8cf78ea1930be0
Vintana <vintanafanteneca@gmail.com>
2024-03-25 02:49:58 +0800

    Updated Player Movement

--- Assets/Scripts/PlayerScript/Player.cs
+++ Assets/Scripts/PlayerScript/Player.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
+    //Call components.
     Rigidbody2D RB;
     Animator Anim;
     SpriteRenderer SR;
-    private bool IsPlayingWalk;
+
+    //Public
     public AudioClip walk;
-    private AudioSource aud;
     public float MoveSpeed;
+
+    //Private
     private float Hmove, Vmove;
+    private bool IsPlayingWalk;
+    private AudioSource aud;
 
     void Start()
     {
@@ -28,23 +34,27 @@ public class PlayerMovement : MonoBehaviour
         //Player Movement
         Hmove = Input.GetAxisRaw("Horizontal");
         Vmove = Input.GetAxisRaw("Vertical");
-        RB.velocity = new Vector2(Hmove * MoveSpeed, Vmove * MoveSpeed);
+        Vector2 NormalizedMove = new Vector2(Hmove, Vmove).normalized; //Normalizes Vector before multiplication.
+        RB.velocity = NormalizedMove*MoveSpeed;
 
         //Updates Animator Component Variables
-        Anim.SetFloat("MoveVAxis", RB.velocity.y);
-        Anim.SetFloat("MoveXAxis", RB.velocity.x);
+        Anim.SetFloat("VAxis", RB.velocity.y);
+        Anim.SetFloat("HAxis", RB.velocity.x);
         
         //Play sound when walking
         if(!IsPlayingWalk && (RB.velocity.x !=0 || RB.velocity.y != 0)) playsound(walk);
         else if (RB.velocity.x == 0 && RB.velocity.y == 0) stopsound();
 
         //Flips sprite animation when moving the opposite way.
-        if(RB.velocity.x>0) SR.flipX = true;
-        else if(RB.velocity.x<0) SR.flipX = false;
+        if(RB.velocity.x<0) SR.flipX = true;
+        else if(RB.velocity.x>0) SR.flipX = false;
         else if(RB.velocity.y>0 && RB.velocity.x == 0 || RB.velocity.y<0 && RB.velocity.x == 0) SR.flipX = false;
     }
 
     private void playsound(AudioClip e){
+        //This function creates a new audiosource whenever the player is walking.
+        //Will play the public audioclip file.
+        //will only play a sound when IsPlayingWalk=false
         aud = gameObject.AddComponent<AudioSource>();
         aud.clip = e;
         aud.Play();
@@ -53,6 +63,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void stopsound(){
+        //destroy audiosource when the player is not walking anymore.
         Destroy(aud);
         IsPlayingWalk = false;
     }
